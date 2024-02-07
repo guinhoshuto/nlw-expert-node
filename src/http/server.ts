03a628283628adf33492294f3a2b8b403284a93e
Guilherme Shuto <guinho.shuto@gmail.com>
2024-02-07 11:53:18 -0400

    vote

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,22 @@
 import fastify from 'fastify'
 import dotenv from 'dotenv'
 import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
+import cookie from '@fastify/cookie'
 
 dotenv.config()
 
 const app = fastify()
+
+app.register(cookie, {
+    secret: "@Eej39ejwi9",
+    hook: 'onRequest',
+})
+
 app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 const port = process.env.PORT || 3333
 
